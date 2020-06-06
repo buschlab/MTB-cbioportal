61a8fe8b11cf05e1fa26e700dfcc70afe1a716e6
Niklas Reimer <niklas@nr205.de>
2020-06-06 14:14:14 +0000

    download recent cgds sql file

--- data/download_seed_database.sh
+++ data/download_seed_database.sh
@@ -1,2 +1,2 @@
 # Download the seed database
-wget "https://raw.githubusercontent.com/cBioPortal/cbioportal/v2.0.0/db-scripts/src/main/resources/cgds.sql" && wget "https://github.com/cBioPortal/datahub/raw/master/seedDB/seed-cbioportal_hg19_v2.7.3.sql.gz"
+wget "https://github.com/nr23730/cbioportal/raw/therapy_recommendation/db-scripts/src/main/resources/cgds.sql" && wget "https://github.com/cBioPortal/datahub/raw/master/seedDB/seed-cbioportal_hg19_v2.7.3.sql.gz"
