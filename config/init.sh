3a435d01c07edb7af0e34b4f7092665c3041a4ab
Niklas Reimer <niklas@nr205.de>
2020-08-07 20:33:10 +0000

    [cbioportal] add: config using init.yml

--- config/init.sh
+++ config/init.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-export $(cat ./../.env | sed 's/#.*//g' | xargs)
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-docker run --rm -it docker.miracum.org/cbioportal/cbioportal:latest cat /cbioportal-webapp/WEB-INF/classes/portal.properties | \
+cat /cbioportal-webapp/WEB-INF/classes/portal.properties | \
     sed 's/db.host=.*/db.host=cbioportal_database:3306/g' | \
     sed 's/db.user=.*/db.user=cbio/g' | \
     sed 's/db.password=.*/db.password='"$MYSQL_USER_PASSWORD"'/g' | \
