34d8fbb45bec0580805d9b946330267042fd5825
Niklas Reimer <niklas@nr205.de>
2020-08-13 11:09:00 +0000

    [misc] add: pull fresh nginx

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 if [ $1 == "cbioportal" ];
 then
   docker build --no-cache --pull -t docker.miracum.org/cbioportal/$1:${RELEASE:-latest} ./services/$1
+  docker pull nginx:stable-alpine
   docker build --no-cache -t docker.miracum.org/cbioportal/cbioproxy:${RELEASE:-latest} ./services/cbioproxy
 elif [ $1 == "cbioproxy" ];
 then
+  docker pull nginx:stable-alpine
   docker build --no-cache -t docker.miracum.org/cbioportal/cbioproxy:${RELEASE:-latest} ./services/cbioproxy
 else
   docker build --no-cache --pull -t docker.miracum.org/cbioportal/$1:${RELEASE:-latest} ./services/$1
