080122623d873923df47993823995cd45b33191d
Niklas Reimer <n.reimer@uni-luebeck.de>
2024-01-10 14:25:06 +0100

    Upgrade to Postgres 16

--- upgradePostgres.sh
+++ upgradePostgres.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
+
+echo 'Which container environment are you using? {docker, podman}'
+read CONTAINER
+
 echo 'Extracting HAPI database from dump'
 sed '/\\connect hapi/,$!d' dump.sql > dump_hapi.sql
 echo 'Stopping an deleting hapiserver and hapi-postgres containers'
-docker-compose stop hapiserver hapi-postgres && docker-compose rm -f hapiserver hapi-postgres
+$CONTAINER-compose stop cbioproxy hapiserver hapi-postgres
+$CONTAINER rm -f cbioproxy_container
+$CONTAINER rm -f cbioportal_fhirspark
+$CONTAINER rm -f cbioportal_fhirspark_hapiserver
+$CONTAINER rm -f cbioportal_fhirspark_database
 echo 'Removing volume from hapi-postgres'
-docker volume rm -f miracum-cbioportal_fhir_data
+$CONTAINER volume rm -f miracum-cbioportal_fhir_data
 echo 'Starting hapi-postgres'
-docker-compose up -d hapi-postgres
+$CONTAINER-compose up -d hapi-postgres
 echo 'Waiting for hapi-postgres to come up'
 sleep 20
 echo 'Importing dumped HAPI database'
-docker-compose exec -T hapi-postgres psql -U hapiserver -d hapi < dump_hapi.sql
-rm dump_hapi.sql
+$CONTAINER-compose exec -T hapi-postgres psql -U hapiserver -d hapi < dump_hapi.sql
 echo 'Starting hapiserver'
-docker-compose up -d hapiserver
+$CONTAINER-compose up -d hapiserver cbioproxy
