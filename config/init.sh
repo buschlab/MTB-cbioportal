3d13fef91827340f5db00459469888735fddc9f0
Niklas Reimer <n.reimer@uni-luebeck.de>
2024-01-17 18:38:52 +0100

    replace saml with oauth2

--- config/init.sh
+++ config/init.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-cat /cbioportal-webapp/application.properties | \
+cat /cbioportal-webapp/application.properties.EXAMPLE | \
     sed 's/spring.datasource.username=.*/spring.datasource.username=cbio/g' | \
     sed 's/spring.datasource.password=.*/spring.datasource.password='"$MYSQL_USER_PASSWORD"'/g' | \
     sed 's|spring.datasource.url=.*|spring.datasource.url=jdbc:mysql://cbioportal_database:3306/cbioportal|g' \
-> portal.properties
+> application.properties
+cp /cbioportal-webapp/security.properties.EXAMPLE security.properties
--- services/cbioproxy/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/ash
-cp /certs/* /usr/local/share/ca-certificates/
-rm /usr/local/share/ca-certificates/.gitkeep
-update-ca-certificates
-/usr/local/openresty/bin/openresty -g "daemon off;"
