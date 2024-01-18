0863a40223f10c1d4f831b9eda77d074d3783bbd
Niklas Reimer <n.reimer@uni-luebeck.de>
2024-01-18 17:25:50 +0100

    autoconfig for keycloak certificate

--- config/init.sh
+++ config/init.sh
@@ -6,3 +6,5 @@ cat /cbioportal-webapp/application.properties.EXAMPLE | \
     sed 's|spring.datasource.url=.*|spring.datasource.url=jdbc:mysql://cbioportal_database:3306/cbioportal|g' \
 > application.properties
 cp /cbioportal-webapp/security.properties.EXAMPLE security.properties
+cp /etc/ssl/certs/java/cacerts cacerts
+keytool -import -trustcacerts -keystore cacerts -storepass changeit -noprompt -alias keycloak -file keycloak.cer
